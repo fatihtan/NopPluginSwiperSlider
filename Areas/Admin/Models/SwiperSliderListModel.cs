12902ddc49056b339406d5f65d704ba532de402f
Fatih TAN <fatihtann@msn.com>
2022-06-01 11:07:32 +0300

    SwiperSliderListModel
    Derivation

--- Areas/Admin/Models/SwiperSliderListModel.cs
+++ Areas/Admin/Models/SwiperSliderListModel.cs
@@ -7,7 +7,7 @@ using Nop.Web.Framework.Models;
 
 namespace Nop.Plugin.Widgets.SwiperSlider.Areas.Admin.Models
 {
-    public record SwiperSliderListModel
+    public record SwiperSliderListModel : BasePagedListModel<SwiperSliderModel>
     {
     }
 }
