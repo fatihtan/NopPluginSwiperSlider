09f7dd33839f38198af726f0e7201bbde94af25f
Fatih TAN <fatihtann@msn.com>
2022-06-01 11:09:33 +0300

    SwiperSliderModel
    Interface Implementation

--- Areas/Admin/Models/SwiperSliderModel.cs
+++ Areas/Admin/Models/SwiperSliderModel.cs
@@ -7,7 +7,40 @@ using Nop.Web.Framework.Models;
 
 namespace Nop.Plugin.Widgets.SwiperSlider.Areas.Admin.Models
 {
-    public class SwiperSliderModel : BaseNopEntityModel, IStoreMappingSupportedModel, IAclSupportedModel
+    public partial record SwiperSliderModel :
+        BaseNopEntityModel,
+        IStoreMappingSupportedModel,
+        IAclSupportedModel
     {
+        public SwiperSliderModel()
+        {
+            SelectedCustomerRoleIds = new List<int>();
+            AvailableCustomerRoles = new List<SelectListItem>();
+
+            SelectedStoreIds = new List<int>();
+            AvailableStores = new List<SelectListItem>();
+            SwiperSliderItemSearchModel = new SwiperSliderItemSearchModel();
+        }
+
+        [NopResourceDisplayName("Nop.Plugin.Widgets.SwiperSlider.Admin.Sliders.Fields.Name")]
+        public string Name { get; set; }
+
+        [NopResourceDisplayName("Nop.Plugin.Widgets.SwiperSlider.Admin.Sliders.Fields.DisplayOrder")]
+        public int DisplayOrder { get; set; }
+
+        [NopResourceDisplayName("Nop.Plugin.Widgets.SwiperSlider.Admin.Sliders.Fields.Published")]
+        public bool Published { get; set; }
+
+        //ACL (customer roles)
+        [NopResourceDisplayName("Nop.Plugin.Widgets.SwiperSlider.Admin.Sliders.Fields.AclCustomerRoles")]
+        public IList<int> SelectedCustomerRoleIds { get; set; }
+        public IList<SelectListItem> AvailableCustomerRoles { get; set; }
+
+        // Store mapping
+        [NopResourceDisplayName("Nop.Plugin.Widgets.SwiperSlider.Admin.Sliders.Fields.LimitedToStores")]
+        public IList<int> SelectedStoreIds { get; set; }
+        public IList<SelectListItem> AvailableStores { get; set; }
+
+        public SwiperSliderItemSearchModel SwiperSliderItemSearchModel { get; set; }
     }
 }
