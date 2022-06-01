c79f8ec75b9fd3cda4bf9d9be8dd825b50744fbc
Fatih TAN <fatihtann@msn.com>
2022-06-01 11:10:03 +0300

    SwiperSliderModel
    Done

--- Areas/Admin/Models/SwiperSliderModel.cs
+++ Areas/Admin/Models/SwiperSliderModel.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Nop.Web.Framework.Models;
+using Nop.Web.Framework.Mvc.ModelBinding;
 
 namespace Nop.Plugin.Widgets.SwiperSlider.Areas.Admin.Models
 {
