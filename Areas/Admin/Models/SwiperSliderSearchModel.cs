053dc6fed00f1a70148616cf3576c483d70c0bb3
Fatih TAN <fatihtann@msn.com>
2022-06-02 09:44:57 +0300

    SwiperSliderSearchModel

---
+++ Areas/Admin/Models/SwiperSliderSearchModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Nop.Web.Framework.Models;
+using Nop.Web.Framework.Mvc.ModelBinding;
+
+namespace Nop.Plugin.Widgets.SwiperSlider.Areas.Admin.Models
+{
+    public record SwiperSliderSearchModel : BaseSearchModel, IPagingRequestModel
+    {
+        #region Ctor
+
+        public SwiperSliderSearchModel()
+        {
+            AvailableStores = new List<SelectListItem>();
+            AvailablePublishedOptions = new List<SelectListItem>();
+        }
+
+        #endregion
+
+        #region Properties
+
+        [NopResourceDisplayName("Nop.Plugin.Widgets.SwiperSlider.Sliders.List.SearchSliderName")]
+        public string SearchSliderName { get; set; }
+
+        [NopResourceDisplayName("Nop.Plugin.Widgets.SwiperSlider.Sliders.List.SearchPublished")]
+        public int SearchPublishedId { get; set; }
+
+        public IList<SelectListItem> AvailablePublishedOptions { get; set; }
+
+        [NopResourceDisplayName("Nop.Plugin.Widgets.SwiperSlider.Sliders.List.SearchStore")]
+        public int SearchStoreId { get; set; }
+
+        public IList<SelectListItem> AvailableStores { get; set; }
+
+        public bool HideStoresList { get; set; }
+
+        #endregion
+    }
+}
