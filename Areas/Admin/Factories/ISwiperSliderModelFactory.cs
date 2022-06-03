9f41e63bef0f33c9f004719fc4487141f11243a3
Fatih TAN <fatihtann@msn.com>
2022-06-03 09:53:33 +0300

    ISwiperSliderModelFactory

---
+++ Areas/Admin/Factories/ISwiperSliderModelFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nop.Plugin.Widgets.SwiperSlider.Areas.Admin.Models;
+using Nop.Plugin.Widgets.SwiperSlider.Data.Domain;
+
+namespace Nop.Plugin.Widgets.SwiperSlider.Areas.Admin.Factories
+{
+    public interface ISwiperSliderModelFactory
+    {
+        Task<SwiperSliderSearchModel> PrepareSliderSearchModelAsync(SwiperSliderSearchModel searchModel);
+        Task<SwiperSliderListModel> PrepareSliderListModelAsync(SwiperSliderSearchModel searchModel);
+        Task<SwiperSliderModel> PrepareSliderModelAsync(SwiperSliderModel model, Slider entity);
+
+        Task<SwiperSliderItemSearchModel> PrepareSliderItemSearchModelAsync(SwiperSliderItemSearchModel searchModel, Slider slider);
+        Task<SwiperSliderItemListModel> PrepareSliderItemListModelAsync(SwiperSliderItemSearchModel searchModel);
+        Task<SwiperSliderItemModel> PrepareSliderItemModelAsync(SwiperSliderItemModel model, SliderItem sliderItem);
+    }
+}
