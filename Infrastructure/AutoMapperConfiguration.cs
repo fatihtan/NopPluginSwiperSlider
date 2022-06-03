9cbca4ed5a4d9c1deb8e08cae2af3eb141bbdc5c
Fatih TAN <fatihtann@msn.com>
2022-06-03 09:49:16 +0300

    AutoMapperConfiguration

---
+++ Infrastructure/AutoMapperConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Nop.Core.Infrastructure.Mapper;
+using Nop.Plugin.Widgets.SwiperSlider.Areas.Admin.Models;
+using Nop.Plugin.Widgets.SwiperSlider.Data.Domain;
+
+namespace Nop.Plugin.Widgets.SwiperSlider.Infrastructure
+{
+    public class AutoMapperConfiguration : Profile, IOrderedMapperProfile
+    {
+        public AutoMapperConfiguration()
+        {
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            CreateMap<Slider, SwiperSliderModel>().ReverseMap();
+            CreateMap<SliderItem, SwiperSliderItemModel>().ReverseMap();
+        }
+
+        public int Order => int.MaxValue;
+
+    }
+}
